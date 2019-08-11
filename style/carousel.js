d20fa532b16664f0747348b9c34cb6fcf1882a3d
An Huynh <sora0aozora@gmail.com>
2019-08-12 00:54:10 +0700

    Adding JS file

---
+++ style/carousel.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function slides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("Pics");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
